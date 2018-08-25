852bff5066fb703eda254f898519cae85fa74487
HP <HP@LAB001-23>
2018-08-24 21:41:19 -0300

    0,2

--- src/ultrabrazalho/UltraBrazalho.java
+++ src/ultrabrazalho/UltraBrazalho.java
@@ -1,6 +1,13 @@
 package ultrabrazalho;
 public class UltraBrazalho {
     public static void main(String[] args) {
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
+        System.out.println("sdkjlasdajkl");
     }
     
 }
